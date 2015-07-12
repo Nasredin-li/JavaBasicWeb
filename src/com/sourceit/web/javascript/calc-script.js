994d62f0966b7a33a9f049513dbaf300c167fa91
Sergey <s.prepress@yandex.ru>
2015-07-13 00:34:38 +0300

    Calc

---
+++ src/com/sourceit/web/javascript/calc-script.js
@@ -0,0 +1,16 @@
+var field1=document.getElementById("field1").value;
+var field2=document.getElementById("field2").value;
+var action=document.getElementById("action").value;
+
+var result = function result(action, field1, field2){
+	if(action=="+"){
+	return field1+field2;
+	}
+};
+
+var calculate=document.getElementById("calculate").value;
+
+var result;
+function print(calculate){
+	calculate=innerHTML.result;
+}
