0cc4cc2640911b743ec7c25a0388e645c273208b
Sergey <s.prepress@yandex.ru>
2015-07-17 00:06:47 +0300

    Notes. it`s working

--- WebContent/comment/comment.jsp
+++ WebContent/comment/comment.jsp
@@ -5,15 +5,26 @@
 <head>
 <meta charset="UTF-8">
 <title>Comment</title>
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  	<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+	<script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="comm.css">
 </head>
 <body>
+<div class="container">
 <form method="post">
-	<H1>Hello, who are you?</H1>
+	<H2>Hello, who are you?</H2>
 	<label for="username">Name:</label>
-	<input id="username" name="username" autofocus placeholder="username">
+	<input id="username" name="username" autofocus placeholder="username"></br>
 	<textarea id="textcomment" name="textcomment" autofocus placeholder="your comment"></textarea>
-	<input type="submit" value="Enter">
+	<input type="submit" id="send" value="Enter">
 </form>
+</div>
+</br></br>
+<div class="container">
+  ${notesList}
+</div>
+  
 
  
 </body>
--- src/com/sourceit/web/comment/CommentServlet.java
+++ src/com/sourceit/web/comment/CommentServlet.java
@@ -2,39 +2,50 @@ package com.sourceit.web.comment;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class CommentServlet extends HttpServlet{
-	
+public class CommentServlet extends HttpServlet {
+	StringBuffer list=new StringBuffer();
+	int i=0;
 	@Override
 	public void init() throws ServletException {
 		super.init();
-		
-		
-		
+
 	}
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
 		req.getRequestDispatcher("comment/comment.jsp").forward(req, resp);
-		
+
 	}
-	
+
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
 		String username = req.getParameter("username");
 		String textcomment = req.getParameter("textcomment");
-		PrintWriter out = resp.getWriter();
-		out.print("<p>");
-		out.print(username);
-		out.print(" writes:<br></p>");
-		out.print("<p>");
-		out.print(textcomment);
-		out.print("<br></p>");
-	}
+
+		if (username != "" && textcomment != "") {
+			if(i/2==0){
+				list.append("	");
+				i++;
+			}
+			list.append(username + ": " + textcomment);
+			list.append("</br>");
+			
+		}
 		
+		
+		req.setAttribute("notesList", list);
+		req.getRequestDispatcher("comment/comment.jsp").forward(req, resp);
+
+	}
+
 }
